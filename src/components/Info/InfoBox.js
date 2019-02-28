a472b80e3eb4923fa3cde360248cf533e558c81c
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-28 12:49:54 -0600

    Adds InfoBox

--- src/components/Info/InfoBox.js
+++ src/components/Info/InfoBox.js
@@ -17,7 +17,7 @@ margin: 0px;
 const InfoBox = ({text, title}) => (  
   <InfoContainer>
       <InfoTitle title="a better fundraiser." />
-      <InfoText text="Our streamlined process makes the fundraiser easy on the organizer. Our great straightforward pricing keeps the organization’s profits high. And finally, we have a great-tasting, high-quality product their participants can feel good about selling. Our business is providing organizations with a simple, profitable fundraising system selling steaming hot, freshly-smoked meats and complete meal kits. That's why for more than twenty years, organizations across the Southeast have turned to Firehouse for help with fundraising ideas."/>
+      <InfoText text="Higher profits, lower stress. We're a dedicated fundraiser company selling steaming hot, freshly-smoked meats and complete meal kits. Our streamlined process makes running a Firehouse fundraiser easy on the organizer. Our great straightforward pricing keeps the organization’s profits high. And finally, we have a great-tasting, high-quality product their participants can feel good about selling. That's why for more than twenty years, organizations across the Southeast have turned to Firehouse for help with fundraising ideas."/>
     </InfoContainer>
 );
 
