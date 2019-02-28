08fb553d8db4e3b3b6f75eb42072e4cbc0fb9635
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-28 12:48:05 -0600

    Updates HeaderImage with firewood image

--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -2,13 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import { ParallaxBanner } from 'react-scroll-parallax';
 import crewCropped from "../images/crewCropped.png";
-// import horizontalFlame from "../images/horizontalFlame.png";
+import horizontalFlame from "../images/horizontalFlame.png";
 import firewood from "../images/firewood.png";
 
-const Image = styled.img`
+const HeaderContainer = styled.div`
+  display: flex;
   width: 100%;
-  margin: 0px;
+  /* height: auto; */
+  background-image: url(${firewood});
+  background-size: cover;
+  align-items: center;
+  justify-content: center;
+`;
+
+// const Image = styled.img`
+//   width: 100%;
+//   margin: 0px;
+//   height: auto;
+// `;
+
+const Logo = styled.img`
+  width: 300px;
   height: auto;
+  margin: 0px;
 `;
 
 const HeaderImage = () => (  
@@ -26,7 +42,10 @@ const HeaderImage = () => (
   //   }}
   // >
   // </ParallaxBanner>
-  <Image src={firewood} alt="Firewood" />
+  <HeaderContainer>
+    <Logo src={horizontalFlame} alt="Logo" />
+    {/* <Image src={firewood} alt="Firewood" />  */}
+  </HeaderContainer>
 );
 
 export default HeaderImage;
