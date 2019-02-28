b888a438e3ac6aae6a8da90954ce79b5a8f7c9e1
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-28 08:25:51 -0600

    Adds styled section banner image

--- src/components/SectionImage.js
+++ src/components/SectionImage.js
@@ -1,30 +1,30 @@
 import React from "react";
 import styled from "styled-components"
 import trailer from "../images/trailer.jpg"
-import { ParallaxBanner } from 'react-scroll-parallax'
+// import { ParallaxBanner } from 'react-scroll-parallax'
 
-// const Image = styled.img`
-//   width: 100%;
-//   margin: 0px;
-//   height: auto;
-// `;
+const Image = styled.img`
+  width: 100%;
+  margin: 0px;
+  height: auto;
+`;
 
 const SectionImage = () => (  
-  <ParallaxBanner
-  layers={[
-    {
-      image: trailer,
-      amount: 0.4,
-      slowerScrollRate: false,
-      expanded: true,
-    }
-  ]}
-  style={{
-    height: '300px',
-  }}
-  >
-  </ParallaxBanner>
-  // <Image src={trailer} alt="The Crew" />  
+  // <ParallaxBanner
+  // layers={[
+  //   {
+  //     image: trailer,
+  //     amount: 0.4,
+  //     slowerScrollRate: false,
+  //     expanded: true,
+  //   }
+  // ]}
+  // style={{
+  //   height: '300px',
+  // }}
+  // >
+  // </ParallaxBanner>
+  <Image src={trailer} alt="We're mobile!" />  
 );
 
 
