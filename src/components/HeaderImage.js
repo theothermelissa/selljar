ef161ef17848a55f21f86c786465d230f94426f1
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-02-28 08:22:17 -0600

    Fiddling with the header image styling and structure

--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,30 +1,32 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 import { ParallaxBanner } from 'react-scroll-parallax';
-import crew from "../images/crew.jpg";
+import crewCropped from "../images/crewCropped.png";
 // import horizontalFlame from "../images/horizontalFlame.png";
+import firewood from "../images/firewood.png";
 
-// const Image = styled.img`
-//   width: 100%;
-//   margin: 0px;
-//   height: auto;
-// `;
+const Image = styled.img`
+  width: 100%;
+  margin: 0px;
+  height: auto;
+`;
 
 const HeaderImage = () => (  
-  <ParallaxBanner
-    layers={[
-      {
-        image: crew,
-        amount: 0.4,
-        slowerScrollRate: false,
-        expanded: true,
-      },
-    ]}
-    style={{
-      height: '500px',
-    }}
-  >
-  </ParallaxBanner>
+  // <ParallaxBanner
+  //   layers={[
+  //     {
+  //       image: crewCropped,
+  //       amount: 0.4,
+  //       slowerScrollRate: false,
+  //       expanded: true,
+  //     },
+  //   ]}
+  //   style={{
+  //     height: '500px',
+  //   }}
+  // >
+  // </ParallaxBanner>
+  <Image src={firewood} alt="Firewood" />
 );
 
 export default HeaderImage;
